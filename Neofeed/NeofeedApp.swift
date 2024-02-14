ddecc221610dde3c6bfd2fa4d980e7cba78886e5
laetuz <laetuzg@gmail.com>
2024-02-15 05:34:38 +0700

    Initial Commit

---
+++ Neofeed/NeofeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeofeedApp.swift
+//  Neofeed
+//
+//  Created by Ryo Martin on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NeofeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
