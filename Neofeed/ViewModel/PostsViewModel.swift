6d99f19c7238d1c1f12fcd23a69a8cd8a07c5d9f
laetuz <laetuzg@gmail.com>
2024-02-21 11:19:46 +0700

    Added new post button and its implementation.

---
+++ Neofeed/ViewModel/PostsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostsViewModel.swift
+//  Neofeed
+//
+//  Created by Ryo Martin on 21/02/24.
+//
+
+import Foundation
---
+++ Neofeed/Views/NewPostForm.swift
@@ -0,0 +1,18 @@
+//
+//  NewPostForm.swift
+//  Neofeed
+//
+//  Created by Ryo Martin on 18/02/24.
+//
+
+import SwiftUI
+
+struct NewPostForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewPostForm()
+}
